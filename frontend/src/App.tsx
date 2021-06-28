7f173646bc82d31d50acfc2c687bce8307a071c5
snenene <sennebels@gmail.com>
2021-06-29 00:22:16 +0200

    Basic Routing + Styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+import Login from "./Components/Authentication/Login.Component";
+import Chat from "./Components/Chat/Chat.Component";
+import Dashboard from "./Components/Dashboard/Dashboard.Component";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      {/* imma make a cool thing here */}
+    <div>
+      <Router>
+      <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/chat">
+            <Chat />
+          </Route>
+          <Route exact path="/">
+            <Dashboard />
+          </Route>
+          
+        </Switch>
+      </Router>
+      <div className="top-decoration">
+        <div className="red-decoration"></div>
+        <div className="orange-decoration"></div>
+        <div className="yellow-decoration"></div>
+      </div>
+      <div className="App">
+        
+      </div>
     </div>
   );
-}
-
-export default App;
+  }
---
+++ frontend/src/Components/Chat/Chat.Component.tsx
@@ -0,0 +1,8 @@
+export default function Chat() {
+    return (
+      <div>
+        <h1>Chat</h1>
+      </div>
+    );
+  }
+  
