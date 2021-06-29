ed60942fa95a651ed0c68dd37a2858e6e45102ae
snenene <sennebels@gmail.com>
2021-06-29 21:46:43 +0200

    Adding ENV file and Login Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import {useState} from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  Redirect
 } from "react-router-dom";
 import './App.css';
 import Login from "./Components/Authentication/Login.Component";
@@ -12,20 +13,19 @@ import Dashboard from "./Components/Dashboard/Dashboard.Component";
 import Settings from "./Components/Settings/Settings.Component";
 
 export default function App() {
+  const [authenticated, setAuthenticated] = useState(false);
+  if (authenticated === true) {
   return (
     <div>
       <Router>
       <div className="App">
       <Switch>
-          <Route path="/login" component={Login}>
-          </Route>
           <Route path="/chat/:recipient" component={Chat}>
           </Route>
           <Route exact path="/settings" component={Settings}>
           </Route>
           <Route exact path="/" component={Dashboard}>
           </Route>
-          
         </Switch>
         </div>
       </Router>
@@ -35,5 +35,28 @@ export default function App() {
         <div className="yellow-decoration"></div>
       </div>
     </div>
-  );
+  )
+}
+  else {
+    return (
+      <div>
+      <Router>
+      <div className="App">
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/login"></Redirect>
+        </Route>
+          <Route path="/login" component={Login}>
+          </Route>
+        </Switch>
+        </div>
+      </Router>
+      <div className="top-decoration">
+        <div className="red-decoration"></div>
+        <div className="orange-decoration"></div>
+        <div className="yellow-decoration"></div>
+      </div>
+    </div>
+    )
+  }
   }
--- frontend/src/Components/Authentication/Login.Component.tsx
+++ frontend/src/Components/Authentication/Login.Component.tsx
@@ -1,7 +1,44 @@
+import {useState} from 'react';
+import axios from 'axios';
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const login = (e : any) => {
+    e.preventDefault();
+    axios.post(`${process.env.REACT_APP_API_URL}/login`, 
+    {
+      email: email,
+      password: password
+    })
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="login-container">
+      <div>
+      <form className="login-form" onSubmit={e => login(e)}>
+        <div>
+        <h1>Welcome!</h1>
+        <p>Meet people from hives all around the world.</p>
+        <p>Share your interests &amp; find new like-minded bees.</p>
+      </div>
+        <div>
+        <input className="login-input" onChange={e => setEmail(e.target.value)} placeholder="EMAIL" ></input>
+        </div>
+        <div>
+        <input className="login-input" onChange={e => setPassword(e.target.value)} placeholder="PASSWORD" ></input>
+        </div>
+        <div>
+        <button className="login-button" type="submit" onClick={e => login(e)}>LOGIN</button>
+        </div>
+      </form>
+      </div>
     </div>
   );
 }
