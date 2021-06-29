14db1905663862b97def9f4ffe6150ed8125285f
snenene <sennebels@gmail.com>
2021-06-29 18:35:02 +0200

    FIxing Styling

--- frontend/src/Components/Chat/Chat.Component.tsx
+++ frontend/src/Components/Chat/Chat.Component.tsx
@@ -11,19 +11,18 @@ const Chat = ({match} : any, props : any) => {
     return (
       <div className="chat-route">
         <DMSidebar/>
-        <div className="recipient-name-container">
-          <div className="recipient-name">{match.params.recipient}</div>
-          </div>
           <form className="chat-container" onSubmit={(e) => sendMessage(e)}>
-        <div>
+          <div className="recipient-name-container">
+          <div className="recipient-name">{match.params.recipient}
+          </div>
+          </div>
         <div className="chat-area">
         </div>
         <div className="input-container">
-        <textarea className="chat-input" placeholder="Type Something..." ></textarea>
+        <span contentEditable className="chat-input" placeholder="Type Something..." ></span>
         <button onClick={(e) => sendMessage(e)} className="chat-button" type="submit">
         <SendRoundedIcon style={{fontSize: 40}} />
         </button>
-        </div>
         </div>
         </form>
       </div>
